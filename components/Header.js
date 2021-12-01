168fb05872b0ffc2f9628b91a1ad7d8e59dfdf78
Ashrf Mathkour Obeidat <ashrfmathkour@gmail.com>
2021-12-01 20:20:15 +0200

    lab39

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,17 @@
 import React from "react";
-import Link from "next/link";     
-  
+import Link from "next/link";
+import { useAuth } from '../contexts/auth'     
 export default function Header() {
+  const { user, login } = useAuth();
   return (
     <div>
 
-      <header className=" flex bg-green-500  w-screen p-0 absolute top-0  font-bold justify-between	">
-        <h1 className=" text-black text-4xl p-4 ">Cookie Stand Admin</h1>
+      <header className="absolute top-0 flex justify-between w-screen p-0 font-bold bg-green-500 ">
+        <h1 className="p-4 text-4xl text-black ">Cookie Stand Admin</h1>
         <Link href='/overview' > 
-        <button className='bg-gray-400 m-4 rounded-lg p-2 font-bold self-end '>overview</button>
+        <button className='self-end p-2 m-4 font-bold bg-gray-400 rounded-lg '>overview</button>
        </Link>
+       <button className="p-2 text-white bg-gray-500 rounded" onClick={() => login("ash288", '1234')}>Login</button>
       </header>
     </div>
   );
